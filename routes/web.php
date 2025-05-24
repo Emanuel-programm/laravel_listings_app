2caad9c1efbcd7dfad3d6f667099549475d434c0
mr wandola <wandogeorge2003@gmail.com>
2025-05-24 10:33:44 +0300

    morning commit

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,8 @@ use App\Http\Controllers\ApplicantController;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
+Route::get('/jobs/search', [JobController::class, 'search'])->name('jobs.search');
+
 // Route::resource('/jobs', JobController::class);
 Route::resource('/jobs', JobController::class)->middleware('auth')->only(['create', 'edit', 'update', 'destroy']);
 Route::resource('/jobs', JobController::class)->except(['create', 'edit', 'update', 'destroy']);
